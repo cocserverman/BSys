7672a361c7637b55d1293fea5e5a3d2bd334efaf
cocserverman <91588916+cocserverman@users.noreply.github.com>
2025-07-20 13:01:54 +0330

    partial account implement

--- Account.h
+++ Account.h
@@ -3,23 +3,21 @@
 
 #include <QObject>
 
-class FinancialAcc : public QObject
-{
-    Q_OBJECT
+class FinancialAcc {
+protected:
+    QString cardNumber;
+    QString iban;
+    QString accountNumber;
+    QString cvv2;
+    QDate expirationDate; //for card
+    double balance;
+    QString pin; //first pin
+    QString staticSecondPass;
+    QString dynamicSecondPass;
 public:
-    explicit FinancialAcc(QObject *parent = nullptr);
-private:
-    string CardNum;
-    string AccNumber;
-    string WireNumber;
-    int CVV2;
-    QDate CardExp;
-    int MainPass;
-    int StaticSecPass;
-    int VariableSecPass;
-    int balance;
 
-signals:
+
+
 };
 
 #endif // ACCOUNT_H
