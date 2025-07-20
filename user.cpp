e2c27783e336610aa0504c338df6b9cbdb932b70
cocserverman <91588916+cocserverman@users.noreply.github.com>
2025-07-20 12:26:26 +0330

    User fix

--- user.cpp
+++ user.cpp
@@ -1,5 +1,21 @@
 #include "user.h"
 
-User::User(QObject *parent)
-    : QObject{parent}
-{}
+User::User(const QString& firstName,const QString& lastName,const QString& nationalID,int age,const QString& username,const QString& password)
+    : firstName(firstName),lastName(lastName),nationalID(nationalID),age(age),username(username),password(password) {}
+
+QString getUsername() const {
+    return username;
+}
+QString getPassword() const {
+    return password;
+}
+QString getFullName() const {
+    return firstName + " " + lastName;
+}
+QString getNationalID() const {
+    return nationalID;
+}
+int getAge() const {
+    return age;
+}
+
